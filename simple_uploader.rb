4fb97d3806fe7d472918dd81f5f1258310d3c37c
Dieterich Lawson <dieterich.lawson@gmail.com>
2012-06-01 10:52:13 -0700

    Added rudimentary error handling
    The server now returns the proper HTTP status codes
    for unsupported methods and badly formatted requests.
    
    This does not protect against badly formatted POST requests
    that include a 'file' parameter, but it's a start.

--- simple_uploader.rb
+++ simple_uploader.rb
@@ -11,8 +11,15 @@ class SimpleUploader < Goliath::API
   #If a file by the specified filename already exists, then
   #the existing file will be overwritten.
   def response(env)
+    if env['REQUEST_METHOD'] != 'POST'
+      return [405,{},"Method Not Allowed - must use POST\n"]
+    end
+    if not env['params'].has_key?('file')
+      return [400,{},"Bad Request - must include a file\n"]
+    end
     upload = env['params']['file'][:tempfile].path
-    new_path = "./uploads/" + env['params']['file'][:filename]
+    new_path = './uploads/' + env['params']['file'][:filename]
+    FileUtils.mkdir 'uploads' unless File.directory?('./uploads')
     FileUtils.move(upload,new_path)
     [200, {}, "Upload Successful\n"]
   end
